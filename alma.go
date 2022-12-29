af90306f8b154c66f69747826957a9e4c171fee9
Sergey Yakubovskiy <sergey.yakubovskiy@cenomi.com>
2022-12-29 14:58:22 +0300

    updated it

--- alma.go
+++ alma.go
@@ -30,3 +30,26 @@ func (c Client) GetAllServices() ([]*sc.Service, error) {
 
 	return serviceCatalogue.Services, nil
 }
+
+// GetService returns meta of Service
+func (c Client) GetService(service string) (*sc.Service, error) {
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/services/%s", c.HostURL, service), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.DoRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// var services []*Service
+	var serviceCatalogue sc.Service
+	err = json.Unmarshal(body, &serviceCatalogue)
+	if err != nil {
+		return nil, err
+	}
+
+	return &serviceCatalogue, nil
+}
